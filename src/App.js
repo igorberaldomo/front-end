aa3f61a5b9571ef588f3d6b54c506f95670604d9
Igor <igorberaldomo@gmai.com>
2025-01-09 16:49:41 -0300

    API

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000'
+});
+function App() {
+  const [data, setData] = useState([]);
+
+  const loaddata = useCallback(async () => {
+    const { data: requestdata } = await axiosInstance.get('/');
+    setData(requestdata);
+    console.log(requestdata);
+  }, [])
+  useEffect(() => {
+    loaddata();
+  }, []);
+  return (
+    <div className="App">
+      <h1>{data?.title}</h1>
+    </div>
+  );
+}
+
+export default App;
